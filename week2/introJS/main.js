a1d9de2b690db63df9dc601276a2fa49d90b00bd
devaio <robert.camp@outlook.com>
2015-09-08 13:39:55 -0600

    functions

--- week2/introJS/main.js
+++ week2/introJS/main.js
@@ -154,39 +154,153 @@ start *= 2
 // }
 
 
-var myObj = {}
+// var myObj = {}
 
-var topGun = {
-    rating : 6.8,
-    genre  : 'Historical autobiography',
-    title  : 'Top Gun',
-    year   : 1986,
-}
-
-
-// console.log(topGun['genre'])
-
-// for ( var key in topGun ) {
-//     console.log('The ' + key + ' of the movie is ' + topGun[key])
+// var topGun = {
+//     rating : 6.8,
+//     genre  : 'Historical autobiography',
+//     title  : 'Top Gun',
+//     year   : 1986,
 // }
 
-// var which = prompt('Which property do you want to know about?')
 
-// console.log(topGun[which])
+// // console.log(topGun['genre'])
 
-var meanGirls = {
-    rating : 'fetch',
-    genre  : 'inspirational documentary',
-    year   : "he doesn't even go here.",
-    title  : "Mean Girls",
+// // for ( var key in topGun ) {
+// //     console.log('The ' + key + ' of the movie is ' + topGun[key])
+// // }
+
+// // var which = prompt('Which property do you want to know about?')
+
+// // console.log(topGun[which])
+
+// var meanGirls = {
+//     rating : 'fetch',
+//     genre  : 'inspirational documentary',
+//     year   : "he doesn't even go here.",
+//     title  : "Mean Girls",
+// }
+
+// topGun.rating = 6.9
+
+// // console.log(topGun)
+
+// var movies = []
+// movies.push(topGun)
+// movies.push(meanGirls)
+// console.log(movies)
+
+// f(x) = x + 1
+// f(7) -> 8
+
+
+// // variable POINTING to an ANONYMOUS function
+// var calculateArea = function (width, height){
+// 	return width * height
+// }
+
+// console.log( 'Literal', calculateArea )
+// console.log( 'Invocation', calculateArea(1,4) )
+// var boxArea = calculateArea(4, 19)
+// console.log('Box Area', boxArea)
+
+// // console.log(calculateArea)
+
+// // NAMED function
+// function calculateVolume(width, height, depth) {
+// 	return width * height * depth
+// }
+
+// console.log( calculateVolume(3, 8, 4) )
+
+
+// Built In Methods
+// A Method is a CALLABLE property
+// console.log('hello'.length) // Length is a PROPERTY
+
+// var myArray = []
+// myArray.push('hello')
+
+// console.log(myArray)
+
+// var myString = 'IM SO QUIET!!!'
+
+// // console.log( myString.toLowerCase() )
+
+// var stringArray = myString.split(' ')
+
+// console.log( stringArray.join(' ') )
+
+
+
+
+// var pluralize = function(thing, number){
+// 	if(number === 1) {
+// 		return thing
+// 		// console.log(thing)
+// 	}
+// 	else{
+		
+// 		// console.log('Before Return')
+// 		return thing + 's'
+// 		// console.log(thing + 's')
+
+// 		// return 'something' // 2 return statements wont work
+// 		// only the first will return values
+
+// 		// No code will execute below a return statement
+
+
+// 	}
+
+// 	// return undefined
+// }
+
+// var singleCat = pluralize('cat', 1)
+// // console.log( pluralize('cat', 6) )
+
+// console.log(singleCat)
+
+
+
+// Scope
+// Functions create SCOPE
+
+// var ampersand = '&'
+// // ampersand = '21390123'
+
+// var concat = function(word1, word2){
+// 	var ampersand = '0000'
+// 	// console.log(word1)
+// 	var output = word1 + ampersand + word2
+// 	ampersand = '---' // This will affect the global variable
+
+// 	return output
+// }
+
+
+// console.log( concat('hot', 'dog') )
+// console.log( concat('macaroni', 'cheese') )
+
+// console.log(ampersand)
+// console.log(word1)
+// console.log(output)
+
+var changeMeBrah = 'party'
+
+console.log('Before : ', changeMeBrah)
+
+// Will change the global variable changeMeBrah
+var changeThatVariable = function() {
+	// var changeMeBrah = 'beer' // With a local variable
+	// being defined with the same name, JS will utilize the
+	// local var, NOT affecting the global var
+	changeMeBrah = changeMeBrah.toUpperCase() + '!'
+	console.log('Inside: ', changeMeBrah)
 }
 
-topGun.rating = 6.9
+changeThatVariable() // Calling the changeThatVariable function
 
-// console.log(topGun)
+console.log('After : ', changeMeBrah)
 
-var movies = []
-movies.push(topGun)
-movies.push(meanGirls)
-console.log(movies)
 
